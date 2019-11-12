2158e391bdb6649c28db0c95dd9614998fb2ceaa
Vincent Jegou <vjegou001@iutbayonne.univ-pau.fr>
2019-11-12 18:45:32 +0100

    Ajout entreprise formation et stage a prostageFirstController

--- src/Controller/ProstageFirstController.php
+++ src/Controller/ProstageFirstController.php
@@ -4,16 +4,41 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProstageFirstController extends AbstractController
 {
     /**
-     * @Route("/prostage/first", name="prostage_first")
+     * @Route("/", name="prostage_first")
      */
     public function index()
     {
-        return $this->render('prostage_first/index.html.twig', [
-            'controller_name' => 'ProstageFirstController',
-        ]);
+        return new Response("<h1>accueil</h1>");
     }
+
+    /**
+     * @Route("/entreprise", name="listeEntreprise")
+     */
+    public function listEntreprise() 
+    {
+        return new Response("<h1>Voici la liste des entreprises</h1>");
+    }
+
+    /**
+     * @Route("/formation", name="listeformation")
+     */
+    public function listformation() 
+    {
+        return new Response("<h1>Voici la liste des formations</h1>");
+    }
+
+    /**
+     * @Route("/stage/{id}", name="listeStage")
+     */
+    public function listeStage(int $id) 
+    {
+        return new Response("<h1>Voici le stage $id</h1>");
+    }
+    
+
 }
